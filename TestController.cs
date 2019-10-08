344085d6031d2e83ff36e7d7e874ce4444e37865
secret <522184309@qq.com>
2019-10-08 14:55:56 +0800

    C#基础

--- TestController.cs
+++ TestController.cs
@@ -39,6 +39,87 @@ namespace sell.Controllers
 
             return new StreamReader(response.GetResponseStream()).ReadToEnd();
         }
+        //获取两个时间之间的分钟/小时
+        public string getCNTime(string matchdate, string contime, int tytime = 0)
+        {
+            var tday = (Convert.ToDateTime(matchdate) - Convert.ToDateTime(contime)).Days;
+            var thou = (Convert.ToDateTime(matchdate) - Convert.ToDateTime(contime)).Hours;
+            var tmin = 0;
+            if (tytime == 0)
+            {
+                tmin = (Convert.ToDateTime(matchdate) - Convert.ToDateTime(contime)).Minutes;
+            }
+            else
+            {
+                tmin = (Convert.ToDateTime(matchdate) - Convert.ToDateTime(contime)).Minutes + 10;
+            }
+            var etime = "";
+            if (tday != 0)
+            {
+                var stday = (tday * 24) + thou;
+                etime = stday + ":" + tmin;
+            }
+            else
+            {
+                etime = thou + ":" + tmin;
+            }
+            var timecha = etime;
+            var strtime = "";
+            if (timecha != "")
+            {
+                if (Convert.ToInt32(timecha.Split(':')[0]) < 0 || Convert.ToInt32(timecha.Split(':')[1]) < 0)
+                {
+                    var th = "";
+                    var tm = "";
+                    var strtimecha = timecha.Split(':');
+                    if (strtimecha[0].Split('-').Length >= 2)
+                    {
+                        th = strtimecha[0].Split('-')[1];
+                    }
+                    else
+                    {
+                        th = strtimecha[0].Split('-')[0];
+                    }
+                    if (strtimecha[1].Split('-').Length >= 2)
+                    {
+                        tm = strtimecha[1].Split('-')[1];
+                    }
+                    else
+                    {
+                        tm = strtimecha[1].Split('-')[0];
+                    }
+                    strtime = tm;
+                    //strtime = "赛后" + th + "h" + " " + tm + "m";
+                }
+                else
+                {
+                    strtime = timecha.Split(':')[1];
+                    //strtime = "赛前" + timecha.Split(':')[0] + "h" + " " + timecha.Split(':')[1] + "m";
+                }
+
+            }
+            return strtime;
+        }
+        //时间转换
+        public static string getConvertTime(string time, int type = 0)
+        {
+            var endtime = "";
+            if (type != 2)
+            {
+                string format = "ddd MMM dd HH:mm:ss CST yyyy";
+                DateTime d = DateTime.ParseExact(time, format, System.Globalization.CultureInfo.InvariantCulture);
+                endtime = d.ToString("yyyy-MM-dd HH:mm:ss");
+            }
+            else
+            {
+                DateTime dtStart = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+                long lTime = long.Parse(time + "0000");
+                TimeSpan toNow = new TimeSpan(lTime);
+                endtime = dtStart.Add(toNow).ToString("yyyy-MM-dd HH:mm:ss");
+            }
+
+            return endtime;
+        }
        
         
      
